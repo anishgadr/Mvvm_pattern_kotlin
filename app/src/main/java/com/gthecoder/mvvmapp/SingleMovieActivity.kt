90a49ff1db4dec347c93c5e2fc9183473e18a0e5
Anish G <anish@masalabox.com>
2020-12-19 17:39:40 +0530

    code commit

---
+++ app/src/main/java/com/gthecoder/mvvmapp/SingleMovieActivity.kt
@@ -0,0 +1,11 @@
+package com.gthecoder.mvvmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingleMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
